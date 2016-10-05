e41949b9f7c66dafa35d594c6a8a88545c586e97
Cheng Pan <cproripop@gmail.com>
2016-10-05 05:01:43 -0400

    Remove static folder

--- starter/settings.py
+++ starter/settings.py
@@ -135,9 +135,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+#)
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
