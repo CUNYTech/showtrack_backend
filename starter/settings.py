553dda1e9e52f80d6771df4e29e2ce48e6ddd159
Cheng Pan <cproripop@gmail.com>
2016-10-05 14:54:27 -0400

    Add templates

--- starter/settings.py
+++ starter/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'starter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- starter/views.py
+++ starter/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello_world(request):
-    return HttpResponse("Hello World")
+    # return HttpResponse("Hello World")
+    return render(request, 'index.html')
