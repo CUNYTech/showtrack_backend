1770966cd060bae76b208581a1791f707fe4c438
Cheng <cproripop@gmail.com>
2016-10-05 20:06:09 -0400

    Add Trakt api

--- starter/views.py
+++ starter/views.py
@@ -1,6 +1,24 @@
-# from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse
+# from django.shortcuts import render
+from urllib.request import Request, urlopen
+import json
+
+with open('env.json') as data_file:    
+    data = json.load(data_file)
+    CLIENT_ID = data["CLIENT_ID"]
 
 def hello_world(request):
+    
     # return HttpResponse("Hello World")
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    print(data['CLIENT_ID'])
+    headers = {
+    'Content-Type': 'application/json',
+    'trakt-api-version': '2',
+    'trakt-api-key': CLIENT_ID
+    }
+    request = Request('https://api.trakt.tv/search/movie?query=tron', headers=headers)
+
+    response_body = urlopen(request).read()
+    print(response_body)
+    return HttpResponse(response_body)
