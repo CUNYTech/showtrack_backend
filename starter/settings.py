9c654f97d1cd963cb7de4596b2313d8a0af573e3
Cheng Pan <cproripop@gmail.com>
2016-10-12 03:39:20 -0400

    Edit setting

--- starter/settings.py
+++ starter/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # REST Framework
 REST_FRAMEWORK = {
