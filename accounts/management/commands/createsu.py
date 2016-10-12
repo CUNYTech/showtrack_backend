7ffb130829200a8276ada1f8feb658b37e7db789
Cheng Pan <cproripop@gmail.com>
2016-10-12 01:31:48 -0400

    Edit supersu

--- accounts/management/commands/createsu.py
+++ accounts/management/commands/createsu.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            User.objects.create_superuser("admin", "admin@admin.com", "admin", "admin")
